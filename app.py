c7cb847143ae135fae3c180d19afa69607c21f34
venisprajapati <venisprajapati2102@gmail.com>
2021-09-15 18:57:37 +0530

    updated app.py file #4.2

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+"""
+Project Name: YouTube Transcript Summarizer
+YouTube Transcript Summarizer API
+"""
+
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
 
@@ -38,34 +44,53 @@ def respond():
     # For debugging
     # print(f"got name {v_id}")
 
-    response = {}
+    body = {}
+    data = {}
 
     # Check if user doesn't provided  at all
     if not v_id:
-        response["response"] = 200
-        response["error"] = "no video id found, please provide valid video id."
+        data['message'] = "Failed"
+        data["error"] = "no video id found, please provide valid video id."
     
     # Check if the user entered a invalid instead video_id
     elif str(v_id) == "False":
-        response["response"] = 200
-        response["error"] = "video id invalid, please provide valid video id."
+        data['message'] = "Failed"
+        data["error"] = "video id invalid, please provide valid video id."
     
     # Now the user has given a valid video id
     else:
-        response["response"] = 200
-        response['message'] = f"Welcome {v_id} to our awesome platform."
+        data['message'] = "Success"
+        data['id'] = v_id
+        data['count'] = 0
+    
+    body["data"] = data
 
     # Return the response in json format
-    return jsonify(response)
+    return buildResponse(200, body)
+
 
 # Welcome message to our server
 @app.route('/')
 def index():
 
-    response = {}
-    response["response"] = 200
-    response['message'] = "Welcome to YTS server."
-    return jsonify(response)
+    body = {}
+    body['message'] = "Success"
+    body['data'] = "Welcome to YTS API."
+
+    return buildResponse(200, body)
+
+
+def buildResponse(statusCode, body):
+
+    res = {
+            "statusCode": statusCode,
+		    "headers": {
+			"Content-Type": "application/json",
+		    },
+		    "body": body,
+        }
+    return jsonify(res)
+
 
 if __name__ == '__main__':
 
