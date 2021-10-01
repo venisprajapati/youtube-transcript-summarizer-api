c6d2d95a2af22fb09b6ce6f0f971a79608e033f1
venisprajapati <venisprajapati2102@gmail.com>
2021-10-01 23:05:21 +0530

    API fully Updated with NLP model and backend # production-ready #1

--- model.py
+++ model.py
@@ -1,40 +1,90 @@
-from transformers import pipeline
+import re
+import nltk
+import spacy
+from string import punctuation
 from youtube_transcript_api import YouTubeTranscriptApi
 
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
-model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-cnn-12-6")
+nltk.download('stopwords')
 
-def summarizer_model(video_id):
-  YouTubeTranscriptApi.get_transcript(video_id)
-  transcript = YouTubeTranscriptApi.get_transcript(video_id)
-  transcript
 
-  result=""
-  for i in transcript:
-    result += ' '+ i['text']
-  print(len(result))
+def text_summarizer(text):
 
-  summarizer = pipeline('summarization')
+    from heapq import nlargest
+    from nltk.corpus import stopwords
 
-  num_iters = int(len(result)/1000)
-  summarized_text = [] 
-  total_len = 0
-  for i in range(0,num_iters+1):
-    start = 0
-    start = i*1000
-    end = (i+1)*1000
-    out = summarizer(result[start:end])
-    out = out[0]
-    out = out['summary_text']
-    summarized_text.append(out)
-    total_len += len(out)
+    nlp = spacy.load('en_core_web_sm')
+    stop_words = stopwords.words('english')
 
-    script = ""
-    for i in summarized_text:
-      script += i.strip()
+    doc = nlp(text)
+    # tokens=[token.text for token in doc]
 
-  return script
-  # print(summarized_text)
-  # print(total_len)
+    word_frequencies = {}
+    for word in doc:
+        if word.text.lower() not in stop_words:
+            if word.text.lower() not in punctuation:
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
+                else:
+                    word_frequencies[word.text] += 1
 
+    max_frequency = max(word_frequencies.values())
+    for word in word_frequencies.keys():
+        word_frequencies[word] = word_frequencies[word]/max_frequency
+
+    sentence_tokens = [sent for sent in doc.sents]
+
+    sentence_scores = {}
+    for sent in sentence_tokens:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word.text.lower()]
+                else:
+                    sentence_scores[sent] += word_frequencies[word.text.lower()]
+
+    select_length = int(len(sentence_tokens)*0.3)
+    summary = nlargest(select_length, sentence_scores, key=sentence_scores.get)
+
+    summary = [re.sub('[.]', '', (word.text).capitalize().replace(
+        "\n", ",").strip()) for word in summary]
+    final_text = '. '.join(summary)
+
+    final_summary = re.sub(',,|,\.|,\-|[\"]', '', final_text)
+
+    return final_summary
+
+
+def nlp_model(v_id):
+
+    transcript = YouTubeTranscriptApi.get_transcript(v_id)
+
+
+    transcript_size = len(transcript)
+
+    original_text = ' '.join([t['text'] for t in transcript])
+    original_text_length = len(original_text)
+
+
+    s_t = []
+
+    result = ""
+
+    for txt in range(0, transcript_size):
+        if (txt != 0 and txt % 100 == 0):
+            result += ' ' + transcript[txt]['text']
+            s_t.append(text_summarizer(result))
+            result = ""
+        else:
+            result += ' ' + transcript[txt]['text']
+
+        if (txt == transcript_size - 1):
+            result += ' ' + transcript[txt]['text']
+            s_t.append(text_summarizer(result))
+
+    final_smy = ' '.join(s_t) + '.'
+    final_summary_length = len(final_smy)
+
+    # print(original_text_length, '-->', final_summary_length)
+    # print(final_smy)
+
+    return original_text_length, final_summary_length, final_smy
